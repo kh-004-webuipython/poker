d9e0852bdcd9c4c1390b62a670397d6893f2b326
egepsihora <zeland18@ukr.net>
2017-03-29 13:47:51 +0300

    check db

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 room_db = PyMongo(app)
 
+
 def create_room_db(issue_json):
     room = room_db.db.rooms
-    q = room.find_one({'project_id': issue_json["project_id"]})
+    q = room.find_one({'project_id': int(issue_json["project_id"])})
     # room.update_one(q, {'$set': issue_json}, upsert=True)
     if not q:
         issue_json['issues'] = []
@@ -31,27 +32,31 @@ def create_room_db(issue_json):
 
 def read_room_db(project_id):
     room = room_db.db.rooms
-    q = room.find_one({'project_id': project_id}, {'issues': 1, '_id': 0})
-    global state
-    room_name = 'room_500'
-    state[room_name] = dict()
-    issue_list = []
-    for issue in q['issues']:
-        issue_list.append({'id': issue['id'],
-                           'title': issue['title'],
-                           'description': issue['description'],
-                           'estimation': issue['estimation']})
-    user_list = []
-    for teammate in q['team']:
-        user_list.append({'id': teammate['id'],
-                          'name': teammate['title'],
-                          'role': teammate['role'],
-                          'current_vote': teammate['current_vote']})
-    state = {
-        "user_list": user_list,
-        "issue_list": issue_list,
-        "chat_log": []
-    }
+    p_id = int(project_id)
+    try:
+        q = room.find_one({'project_id': p_id}, {'issues': 1, '_id': 0})
+        global state
+        room_name = project_id
+        state[room_name] = dict()
+        issue_list = []
+        for issue in q['issues']:
+            issue_list.append({'id': issue['id'],
+                               'title': issue['title'],
+                               'description': issue['description'],
+                               'estimation': issue['estimation']})
+        user_list = []
+        for teammate in q['team']:
+            user_list.append({'id': teammate['id'],
+                              'name': teammate['title'],
+                              'role': teammate['role'],
+                              'current_vote': teammate['current_vote']})
+        state = {
+            "user_list": user_list,
+            "issue_list": issue_list,
+            "chat_log": []
+        }
+    except Exception:
+        print ("Can't read database")
     return state
 
 # global room
@@ -75,7 +80,7 @@ def main_room_page(room_name=None):
 # add sockets in our app
 socketio = SocketIO(app)
 
-"""
+
 @app.route('/create_room/', methods=['POST'])
 def create_room():
     issue_json = request.get_json(force='True')
@@ -88,23 +93,28 @@ def create_room():
 def add_issue():
     issue_json = request.get_json(force='True')
     room = room_db.db.rooms
-    q = room.find_one({'project_id': issue_json["project_id"]})
-    room.update_one(q, {'$set': {'issues': issue_json['issues']}}, upsert=True)
+    try:
+        q = room.find_one({'project_id': int(issue_json["project_id"])})
+    except Exception:
+        print ("Can't read database")
+    else:
+        room.update_one(q, {'$set': {'issues': issue_json['issues']}}, upsert=True)
     return redirect('index.html')
 
-"""
+
 @socketio.on('join')
 def on_join(data):
     #username = data['username']
     room = data['room']
     # if user in team:
     join_room(room)
-    emit('start_data', state['room_500'])
+    read_room_db(room)
+    emit('start_data', state[room])
     comment = {}
-    comment['id'] = len(state['room_500']['chat_log']) + 1
+    comment['id'] = len(state[room]['chat_log']) + 1
     comment['body'] = 'user' + ' has entered the room.'
     comment['user'] = 'Server'
-    state['room_500']['chat_log'].append(comment)
+    state[room]['chat_log'].append(comment)
     emit('add_new_comment', comment, room=room)
 
 
@@ -117,111 +127,83 @@ def on_leave(data):
     send('user has left the room.', room=room)
 
 
-state = {
-    'room_500': {
-        'user_list': [
-            {
-                'id': 1,
-                'name': 'phobos',
-                'role': 'developer',
-                'current_vote': ''
-            },
-            {
-                'id': 2,
-                'name': 'scrum_name',
-                'role': 'scrum',
-                'current_vote': ''
-            },
-
-        ],
-        'issue_list': [
-            {
-                'id': 1,
-                'title': 'Fix Email Notification(Issues change)',
-                'description': 'Email notification has to work for: 1) ' +
-                               'Employee was assigned to the issue. 2) ' +
-                               'Employee that was assigned to the issue, ' +
-                               'now is not assigned to the issue. 3) If ' +
-                               'issue was changed in any way, it sends to ' +
-                               'assigned issue employee. if NOTHING is ' +
-                               'changed, do not send anything.',
-                'estimation': '',
-            },
-            {
-                'id': 2,
-                'title': 'Profile access',
-                'description': 'Make access to user profile via dropdown(as it was before) and make it bigger',
-                'estimation': 10,
-            },
-            {
-                'id': 3,
-                'title': 'title3',
-                'description': 'description3',
-                'estimation':'',
-            },
-
-        ],
-        'chat_log': [
-            {
-                'id': 1,
-                'user': 'phobos',
-                'body': ' xxxxxxxxxxxxx'
-            },
-            {
-                'id': 2,
-                'user': 'scrum',
-                'body': 'zzzzzzzz'
-            },
-        ]
-
-    },
-}
-#state = dict()
-
+# state = {
+#     'room_500': {
+#         'user_list': [
+#             {
+#                 'id': 1,
+#                 'name': 'phobos',
+#                 'role': 'developer',
+#                 'current_vote': ''
+#             },
+#             {
+#                 'id': 2,
+#                 'name': 'scrum_name',
+#                 'role': 'scrum',
+#                 'current_vote': ''
+#             },
 #
-# def read_db(project_id):
-#     room = room_db.db.rooms
-#     q = room.find_one({'project_id': project_id}, {'issues': 1, '_id': 0})
-#     global state
-#     room_name = 'room_' + str(project_id)
-#     state[room_name] = {}
-#     issue_list = []
-#     for issue in q['issues']:
-#         issue_list.append({'id': issue['id'],
-#                           'title': issue['title'],
-#                           'description': issue['description'],
-#                           'estimation': issue['estimation']})
-#     user_list = []
-#     for teammate in q['team']:
-#         user_list.append({'id': teammate['id'],
-#                          'name': teammate['title'],
-#                          'role': teammate['description'],
-#                          'current_vote': teammate['estimation']})
-#     state = {
-#         "user_list": user_list,
-#         "issue_list": issue_list,
-#         "chat_log": []
-#     }
-#     return state
-
-#state = dict()
+#         ],
+#         'issue_list': [
+#             {
+#                 'id': 1,
+#                 'title': 'Fix Email Notification(Issues change)',
+#                 'description': 'Email notification has to work for: 1) ' +
+#                                'Employee was assigned to the issue. 2) ' +
+#                                'Employee that was assigned to the issue, ' +
+#                                'now is not assigned to the issue. 3) If ' +
+#                                'issue was changed in any way, it sends to ' +
+#                                'assigned issue employee. if NOTHING is ' +
+#                                'changed, do not send anything.',
+#                 'estimation': '',
+#             },
+#             {
+#                 'id': 2,
+#                 'title': 'Profile access',
+#                 'description': 'Make access to user profile via dropdown(as it was before) and make it bigger',
+#                 'estimation': 10,
+#             },
+#             {
+#                 'id': 3,
+#                 'title': 'title3',
+#                 'description': 'description3',
+#                 'estimation':'',
+#             },
+#
+#         ],
+#         'chat_log': [
+#             {
+#                 'id': 1,
+#                 'user': 'phobos',
+#                 'body': ' xxxxxxxxxxxxx'
+#             },
+#             {
+#                 'id': 2,
+#                 'user': 'scrum',
+#                 'body': 'zzzzzzzz'
+#             },
+#         ]
+#
+#     },
+# }
+state = dict()
 
 
 @socketio.on('add_comment')
 def handle_add_comment(data):
     room = data['room']
     comment = {}
-    comment['id'] = len(state['room_500']['chat_log']) + 1
+    comment['id'] = len(state[room]['chat_log']) + 1
     comment['body'] = data['body']
     comment['user'] = data['user']
-    state['room_500']['chat_log'].append(comment)
+    state[room]['chat_log'].append(comment)
     emit('add_new_comment', comment, room=room)
 
 
 @socketio.on('make_vote')
 def handle_vote(data):
     room = data['room']
-    users = state['room_500']['user_list']
+    users = state[room]['user_list']
     for user in users:
         if user['id'] == int(data['user_id']):
             user['current_vote'] = data['card']
@@ -231,26 +213,26 @@ def handle_vote(data):
 @socketio.on('accept_estimation')
 def handle_accept(data):
     room = data['room']
-    issues = state['room_500']['issue_list']
+    issues = state[room]['issue_list']
     # MAKE REST TO DJANGO AND ON success:
         # DELETE ISSUE IN OUR DB
     for issue in issues:
         if issue['id'] == int(data['issue_id']):
             issue['estimation'] = int(data['estimation'])
 
-    users = state['room_500']['user_list']
+    users = state[room]['user_list']
     for user in users:
         user['current_vote'] = ''
 
-    new_users = state['room_500']['user_list']
-    new_issues = state['room_500']['issue_list']
+    new_users = state[room]['user_list']
+    new_issues = state[room]['issue_list']
     emit('issue_was_estimated', {'users': new_users, 'issues': new_issues}, room=room)
 
 
 @socketio.on('reset_estimation')
 def handle_reset_estimation(data):
     room = data['room']
-    users = state['room_500']['user_list']
+    users = state[room]['user_list']
     for user in users:
         user['current_vote'] = ''
     emit('reset_estimation', users, room=room)
@@ -259,7 +241,7 @@ def handle_reset_estimation(data):
 @socketio.on('skip_estimation')
 def handle_skip_estimation(data):
     room = data['room']
-    users = state['room_500']['user_list']
+    users = state[room]['user_list']
     for user in users:
         user['current_vote'] = ''
     emit('skip_estimation', users, room=room)
