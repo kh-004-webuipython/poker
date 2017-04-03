ede240611c540cf11316ac02c46e274f11ba1527
dsfhhf <yodfhu@ex.com>
2017-04-03 13:08:50 +0300

    fix rooms

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -55,10 +55,9 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var USER_ID = document.querySelector('body').dataset['id'];
+	var USER_ID = document.getElementById('poker-app').dataset['id'];
 	var USER = document.getElementById('poker-app').dataset['name'];
-
-	var ROOM = '';
+    var ROOM = String(document.querySelector('body').dataset['room']);
 	var cardList = [0, 1, 2, 3, 5, 8, 13, 20, 40, 100, '?', 'coffee'];
 	var startUserList = [];
 	var startIssueList = [];
@@ -74,9 +73,9 @@
 	    return 56 * cardList.indexOf(Number(x));
 	}
 
-	if (location.pathname.substr(1, 4) === 'room') {
-	    ROOM = String(location.pathname.replace(/^\/room\/|\/$/g, ''));
-	}
+	//if (location.pathname.substr(1, 4) === 'room') {
+	//    ROOM = String(location.pathname.replace(/^\/room\/|\/$/g, ''));
+	//}
 
 	var socket = io.connect('http://127.0.0.1:5000');
 	socket.on('connect', function () {
